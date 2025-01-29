fea460d40360b7557b1abd7f9c47ebbd3fd444ac
Watthana Peerajing <Watthana_Pee@freewillgroup.com>
2025-01-29 13:08:43 +0700

    initial commit

---
+++ app.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { NuxtLayout } from '#components';
+
+</script>
+
+<!-- <script lang="ts" setup>
+
+useHead({
+  title: 'Home',
+  meta: [
+    { 
+      name: 'description', 
+      content: 'Home Nuxt 3, IT Genius Engineering' 
+    },
+    {
+      name: 'keywords',
+      content: 'Home, Nuxt 3, Learning Nuxt 3'
+    },
+  ],
+})
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <h1 class="text-3xl font-bold text-purple-600 animate-bounce">
+      <Icon name="vscode-icons:file-type-nuxt"></Icon>
+      Hello Nuxt 3
+    </h1>
+  </div>
+</template> -->
+<template>
+  <Head>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+  </Head>
+  
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
---
+++ components/App/Header.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="bg-gray-900 text-white py-5 border-b border-gray-700">
+    <div class="container mx-auto px-4 sm:px-0 py-2 flex flex-col sm:flex-row justify-center gap-3 sm:gap-0 sm:justify-between items-center">
+      <NuxtLink to="/"
+        class="text-2xl font-medium">Nuxt 3 Blog</NuxtLink>
+      <ul class="nav">
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/categories">Categories</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/contact">contact</NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style>
+  ul.nav {
+    @apply flex gap-5 items-center justify-end;
+  }
+
+  ul.nav > li > a {
+    @apply text-gray-300 hover:text-gray-50;
+  }
+
+  ul.nav > li > a.router-link-active {
+    @apply text-blue-300;
+  }
+
+</style>
